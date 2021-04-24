7f1303516c1c826af1d0b467d8aa4d9627d9903c
Nurba <ndaiyrkanov@gmail.com>
2021-04-24 18:54:00 +0300

    Some changes

---
+++ test.py
@@ -0,0 +1,20 @@
+
+list_ = [1, 1, 5, 5, 5]
+
+maxim = max(list_)
+def test(num):
+    for num in list_:
+        k = 2
+        if num == maxim:
+            print(num)
+        else:
+            l = num + k
+            while l == maxim:
+                num + k
+            print(l)
+
+test(5)
+
+
+    
+    
